7ca50dd5346a1a5c206172cfac9da3c3c5e49550
2606710413@qq.com <2606710413@qq.com>
2022-05-05 15:49:15 +0800

    init

---
+++ src/main/java/com/cooldesktop/demo/cooldesktopapplicationdemo/CooldesktopApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cooldesktop.demo.cooldesktopapplicationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooldesktopApplicationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooldesktopApplicationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cooldesktop/demo/cooldesktopapplicationdemo/CooldesktopApplicationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooldesktop.demo.cooldesktopapplicationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooldesktopApplicationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
